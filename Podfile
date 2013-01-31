de9da757dc6307667278dab4b7664e7aabfc4490
Ryan Davies <ryan@ryandavies.net>
2013-01-31 00:53:16 +0000

    Use Specify, Expecta, OCMock, to write specs.

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '5.0'
+
+target 'Specs' do
+  pod 'Specify'
+  pod 'Expecta'
+  pod 'OCMock'
+end
--- Specs/TSCSpec.m
+++ Specs/TSCSpec.m
@@ -6,27 +6,8 @@
 //  Copyright (c) 2013 Ryan Davies. All rights reserved.
 //
 
-#import "TSCSpec.h"
+SpecBegin(TSCSpec)
 
-@implementation TSCSpec
+it(@"should be ok", ^{});
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in Specs");
-}
-
-@end
+SpecEnd
